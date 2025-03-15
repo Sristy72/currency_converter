bceda13a68e3d9c5bd33c8a3a2b2cd8db9ff0db4
isita <eshitamondol72@gmail.com>
2025-03-15 23:52:56 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:currency_converter/screen/currency_convert.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CurrencyConvert(),
+    );
+  }
+}
---
+++ lib/screen/currency_convert.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConvert extends StatefulWidget {
+  const CurrencyConvert({super.key});
+
+  @override
+  State<CurrencyConvert> createState() => _CurrencyConvertState();
+}
+
+class _CurrencyConvertState extends State<CurrencyConvert> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
