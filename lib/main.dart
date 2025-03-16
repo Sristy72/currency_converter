fac3df1fe23c217f65f6eb9eca1f1b5b98b20f54
isita <eshitamondol72@gmail.com>
2025-03-16 20:42:58 +0600

    s

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: Colors.white)
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide(color: Colors.white)
+          )
+        )
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Currency Converter',
       home: CurrencyConvert(),
     );
   }
--- lib/screen/currency_convert.dart
+++ lib/screen/currency_convert.dart
@@ -8,10 +8,143 @@ class CurrencyConvert extends StatefulWidget {
 }
 
 class _CurrencyConvertState extends State<CurrencyConvert> {
+  String fromCurrency = 'USD';
+  String toCurrency = 'EUR';
+  double rate = 0.0;
+  double total = 0.0;
+  TextEditingController _amountController = TextEditingController();
+  List<String> currencies = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Currency Converter",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
 
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(40.0),
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/currency.png",
+                    width: MediaQuery.of(context).size.width / 2,
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 10,
+                ),
+                child: TextField(
+                  cursorColor: Colors.white,
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Amount',
+                    labelStyle: TextStyle(color: Colors.white),
+                  ),
+
+                  onChanged: (value) {
+                    if (value != '') {
+                      setState(() {
+                        double amount = double.parse(value);
+                        total = amount * rate;
+                      });
+                    }
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      child: DropdownButton<String>(
+                        value: fromCurrency,
+                        isExpanded: true,
+                        style: TextStyle(color: Colors.white),
+                        items:
+                            currencies.map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            fromCurrency = newValue!;
+                            //_getRate();
+                          });
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        //_swapCurrencies
+                      },
+                      icon: Icon(Icons.swap_horiz, color: Colors.white),
+                    ),
+
+                    SizedBox(
+                      width: 100,
+                      child: DropdownButton<String>(
+                        value: toCurrency,
+                        isExpanded: true,
+                        style: TextStyle(color: Colors.white),
+                        items:
+                        currencies.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            toCurrency = newValue!;
+                            //_getRate();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              Text('Rate $rate',
+                style: TextStyle(color: Colors.white, fontSize: 20,),),
+
+              SizedBox(height: 20,),
+
+              Text('${total.toStringAsFixed(3)}', style: TextStyle(color: Colors.white, fontSize: 20,),),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
